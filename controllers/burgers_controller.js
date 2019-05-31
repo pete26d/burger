a76e15cb07316dbd9f564bad747f47320f4a738a
Spatula Pete <petewx26@gmail.com>
2019-05-31 17:01:25 -0400

    add views and server.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,17 @@
 var express = require("express");
 var burger = require("../models/burger.js");
 var router = express.Router();
+
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+  
+  burger.selectALL(function(burgerData) {
+    
+    res.render("index", { burger_data: burgerData });
+  });
+});
+
+module.exports = router;
